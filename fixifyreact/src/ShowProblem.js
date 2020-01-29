22ef6a142f2208f8c78dd89078e13b2e5af5457e
Joe Hendy <jahendy12@gmail.com>
2020-01-29 12:02:43 -0700

    continued showproblems

--- fixifyreact/src/ShowProblem.js
+++ fixifyreact/src/ShowProblem.js
@@ -31,7 +31,7 @@ class ShowProblem extends React.Component {
             [e.target.description]: e.target.value
         })
     }
-    handleSubmit = async(e) => {
+    handleProblemSubmit = async(e) => {
         this.setState({
             loading: true,
         })
@@ -41,63 +41,41 @@ class ShowProblem extends React.Component {
             description: this.state.description,
             location: this.state.location, 
             owner: this.state.owner
-        }
+        }}
     }
-
-
-
-
-
-
-    // }    
-    // isFetching = () => {
-    //   this.state.isFetching
-    // }
-    // componentDidMount() {
-    //   this.fetchProblems(`localhost:3000/problem`), {
-    //     method: "GET",
-    //     mode: "cors",
-    //     cache: "no-cache",
-    //     headers: {
-    //         "Content-Type": "application/json"
-    //       },
-    //       body: JSON.stringify(data)
-    //     };
-    //     return response.json();
-    //   };
-    //   render = () => {
-    //     <div>
-    //         <div>
-    //             <Grid
-    //                 textAlign='center'
-    //                 style={{ marginTop: '15em', height: '100%' }}
-    //                 verticalAlign='top'
-    //                 stackable
-    //                     >
-    //                         {/* <Grid.Row>
-    //                             <Button onClick={this.createProblem}>Create New Problem</Button>
-    //                         </Grid.Row> */}
-    //             <Grid.Row>
-    //             <Grid.Column>
-    //                 <h2>{this.state.car}</h2>
-    //                 <h4>{this.state.price}</h4>
-    //                 <h4>{this.state.description}</h4>
-    //                 <h4>{this.state.location}</h4>
-    //                 <h4>{this.state.owner}</h4>
-    //             </Grid.Column>
-    //                         {/* <Grid.Row>
-    //                             <Button onClick={this.editProblem}>Edit Problem</Button>
-    //                         </Grid.Row>  */}     
-    //             </Grid.Row>
-    //             </Grid>
-    //             <Grid 
-    //                 textAlign='center'
-    //                 style={{ marginTop: '15em', height: '100%' }}
-    //                 verticalAlign='top'
-    //                 stackable   
-    //             />  
-    //         </div>     
-    //     </div>
-    // }} 
+      render() {
+        <div>
+            <div>
+                <Grid
+                    textAlign='center'
+                    style={{ marginTop: '15em', height: '100%' }}
+                    verticalAlign='top'
+                    stackable
+                        >
+                            {/* <Grid.Row>
+                                <Button onClick={this.createProblem}>Create New Problem</Button>
+                            </Grid.Row> */}
+                <Grid.Row>
+                <Grid.Column>
+                    <h2>{this.state.car}</h2>
+                    <h4>{this.state.price}</h4>
+                    <h4>{this.state.description}</h4>
+                    <h4>{this.state.location}</h4>
+                    <h4>{this.state.owner}</h4>
+                </Grid.Column>
+                            {/* <Grid.Row>
+                                <Button onClick={this.editProblem}>Edit Problem</Button>
+                            </Grid.Row>  */}     
+                </Grid.Row>
+                </Grid>
+                <Grid 
+                    textAlign='center'
+                    style={{ marginTop: '15em', height: '100%' }}
+                    verticalAlign='top'
+                    stackable   
+                />  
+            </div>     
+        </div>
+    }} 
 
 export default ShowProblem;
